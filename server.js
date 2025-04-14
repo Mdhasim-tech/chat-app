a78bee47f9d196232f1c228a40eba3b8ac8b6b27
Mdhasim-tech <131744384+Mdhasim-tech@users.noreply.github.com>
2025-04-14 15:24:39 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: 'http://localhost:3000', // Your frontend URL
+    origin: 'https://chat-app-nqgp.onrender.com', // Your frontend URL
     methods: ['GET', 'POST'],
   },
 });
@@ -41,6 +41,7 @@ io.on('connection', (socket) => {
   });
 });
 
-server.listen(5000, () => {
-  console.log('🚀 Server running on http://localhost:5000');
+const port = process.env.PORT ;  // Fallback to 5000 for local development
+server.listen(port, () => {
+  console.log(`🚀 Server running on port ${port}`);
 });
